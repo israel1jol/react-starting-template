04742919a4d86f62defe182d98c3f1c7d5e69909
Israel <adigun.jolly@gmail.com>
2023-04-09 13:20:20 -0500

    Working on tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -3,8 +3,12 @@ import { screen, render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 
-test("default", () => {
-    render(<App />);
-    const result = screen.getByText(/New Project/);
-    expect(result).toBeInTheDocument();
+
+describe("Primary start of tests", () => {
+    test("default", () => {
+        render(<App />);
+        const result = screen.getByText(/New Project/);
+        expect(result).toBeInTheDocument();
+    })
 })
+
